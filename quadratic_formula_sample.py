74526879df79965712a2ec0d45be491e95a51d97
kimharry <kimharry0610@gmail.com>
2022-10-13 10:27:18 +0900

    useless files deleted

--- quadratic_formula_sample.py
+++
@@ -1,8 +0,0 @@
-from manim import *
-
-class Test(Scene):
-    def construct(self):
-        equation1 = MathTex(r"\therefore",r"\text{When }",r"ax^2 + bx + c = 0").to_edge(UP, buff = 3)
-        equation13 = MathTex(r"x = \frac{-b \pm \sqrt{b^2-4ac}}{2a}").move_to(DOWN)
-        self.play(Write(equation1), Write(equation13), run_time = 2)
-        self.wait()
